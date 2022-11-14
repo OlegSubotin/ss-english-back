b87077edcb1184c6a04ddd7b2e2955c419443841
Oleh Subotin <subotinoleg0@gmail.com>
2022-11-14 17:59:20 +0200

    Update removeCourse.js
    delete all lessons of course,
    delete all lessons description of course
    delete course

--- controllers/courses/removeCourse.js
+++ controllers/courses/removeCourse.js
@@ -1,8 +1,18 @@
-const { courses: service } = require("../../service");
+const createError = require('http-errors');
+const { courses, lessons } = require("../../service");
 
 const removeCourse = async (req, res) => {
   const { courseId } = req.params;
-  const result = await service.removeCourse(courseId);
+  const course = await courses.getCourseById(courseId);
+  if (!course) {
+    throw createError(404, 'No course with such id');
+  }
+  const courseLesson = await lessons.getAllLessonsByCourseId({ courseId });
+  for (const lesson of courseLesson) {
+    await lessons.removeLesson(lesson.id);
+    await lessons.removeLessonDescription(lesson.id);
+  }
+  const result = await courses.removeCourse(courseId);
   res.status(201).json({
     status: "success",
     code: 201,
